68841d2cdfe42f8788e13e16e2a1e516a193cff2
Niko2357 <niktolka16.1@gmail.com>
2024-03-11 10:48:01 +0100

    added constructor, getter and setter

--- src/Barn.java
+++ src/Barn.java
@@ -1,4 +1,18 @@
 public class Barn {
+    protected double area;
+
+    public Barn(double area) {
+        this.area = area;
+    }
 
 
+
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
 }
