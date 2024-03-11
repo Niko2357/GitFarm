fc12054314c5e5475033636c5f35fa8a1f2794e3
Niko2357 <niktolka16.1@gmail.com>
2024-03-11 10:59:50 +0100

    edited method buy

--- src/Barn.java
+++ src/Barn.java
@@ -1,14 +1,34 @@
+import java.util.ArrayList;
+
 public class Barn {
     protected int bigAn;
     protected int smallAn;
+    protected ArrayList<Animal> animals;
 
     public Barn(int bigAn, int smallAn) {
         this.bigAn = bigAn;
         this.smallAn = smallAn;
+        animals = new ArrayList<>();
     }
 
-    public void buy(Animal an){
-
+    public String buy(Animal an){
+        switch(an.size){
+            case SMALL:
+                if(smallAn < 10){
+                    animals.add(an);
+                    return "You brought this animal  " + an.type;
+                }else{
+                    return "You don't have enough space for this animal.";
+                }
+            case BIG:
+                if(bigAn < 10){
+                    animals.add(an);
+                    return "You brought this animal  " + an.type;
+                }else{
+                    return "You don't have enough space for this animal.";
+                }
+        }
+        return " ";
     }
 
     public void stroke(){
