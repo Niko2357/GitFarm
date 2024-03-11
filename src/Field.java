f2669514b005028134ebf2666f54b19c3ba81b80
Niko2357 <niktolka16.1@gmail.com>
2024-03-11 10:07:47 +0100

    new methods plant, water and harvest in class Field added constructor and getters and setters

--- src/Field.java
+++ src/Field.java
@@ -1,3 +1,63 @@
+import java.sql.SQLOutput;
+
 public class Field {
+    protected double area;
+    protected int maxPlants;
+
+
+
+    public Field(double area, int maxPlants ) {
+        this.area = area;
+        this.maxPlants = maxPlants;
+    }
+
+
+    public void plant(Flower fl){
+        if((area > fl.neededArea || area == fl.neededArea) && maxPlants != 5){
+            area = area - fl.neededArea;
+            maxPlants += 1;
+        } else{
+            System.out.println("You cannot plant right now. You reached maximum number of plants or run out of space.");
+        }
+    }
+
+    public String water(Flower fl){
+        fl.chanceOfGrowth += 3;
+        return "You watered flower  " + fl.name;
+    }
+
+    public String harvest(Flower fl){
+        if(fl.grown){
+            area = area + fl.neededArea;
+            return "You harvested this flower  " + fl.name;
+        }else{
+            return "You cannot harvest this flower, it's not grown.";
+        }
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        if(area > 100){
+            this.area = 100;
+        }else{
+            this.area = area;
+        }
+    }
+
+    public int getMaxPlants() {
+        return maxPlants;
+    }
+
+    public void setMaxPlants(int maxPlants) {
+        if(maxPlants > 5){
+            this.maxPlants = 5;
+        }else{
+            this.maxPlants = maxPlants;
+        }
+    }
+
 
 }
